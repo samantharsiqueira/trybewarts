1f98c727392cf33c03575b4c800a65b84f2c8cc0
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-11-18 11:27:23 +0000

    atualizando o projeto

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const handleLoginBtn = () => {
+  const email = document.getElementsById('login-email');
+  const password = document.getElementById('login-passaword');
+
+  if (email === 'tryber@teste.com' && password === 123456) {
+    alert = 'Olá, Tryber!';
+  } else {
+    alert = 'Email ou senha inválidos';
+  }
+};
+
+handleLoginBtn();
+
+document.getElementById('submit-btn').addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
 const handleDisabledBtn = () => {
   const btn = document.getElementById('submit-btn');
   const acceptTerm = document.getElementById('agreement');
