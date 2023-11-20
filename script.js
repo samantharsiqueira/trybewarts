8692fdd3a09ff697d189059c74873c7c465863f0
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-11-20 13:47:59 +0000

    finalizado

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-document.getElementById('submit-btn').addEventListener('click', (event) => {
-  event.preventDefault();
-});
+const handleLoginBtn = () => {
+  const loginBtn = document.getElementById('login-btn');
+  const password = document.getElementById('login-password');
+  const email = document.getElementById('login-email');
 
-const handleDisabledBtn = () => {
-  const btn = document.getElementById('submit-btn');
-  const acceptTerm = document.getElementById('agreement');
-  acceptTerm.addEventListener('click', () => {
-    if (acceptTerm.checked) {
-      btn.disabled = false;
+  loginBtn.addEventListener('click', () => {
+    if (password.value === '123456' && email.value === 'tryber@teste.com') {
+      alert('Olá, Tryber!');
     } else {
-      btn.disabled = true;
+      alert('Email ou senha inválidos.');
     }
   });
 };
 
+const handleDisabledBtn = () => {
+  const submitBtn = document.getElementById('submit-btn');
+  const acceptTerm = document.getElementById('agreement');
+  acceptTerm.addEventListener('click', () => {
+    if (acceptTerm.checked) {
+      submitBtn.removeAttribute('disabled');
+    }
+  });
+};
+
+handleLoginBtn();
 handleDisabledBtn();
